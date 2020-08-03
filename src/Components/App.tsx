7816c50ca99b603d6f2808573d9fc14acbd2dce2
Kevin Kuchta <kevin@kevinkuchta.com>
2020-08-02 23:20:40 -0700

    Added credits

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -15,6 +15,13 @@ const App = () => {
       ) : (
         <IntroScreen startGame={() => setGameState(Game.start)} />
       )}
+      <div className="text-center py-5 opacity-75 text-sm">
+        <a href="https://twitter.com/evoroxv/status/1267323832945315840">
+          @evoroxv
+        </a>{" "}
+        came up with this game.{" "}
+        <a href="https://twitter.com/kkuchta">@kkuchta</a> just coded it.
+      </div>
     </div>
   );
 };
